0e34c02ba27c3697482d4fa889151f25f5b07ad0
jwr1 <47087725+jwr1@users.noreply.github.com>
2025-04-17 21:48:31 -0400

    Add GitHub page deployments

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://interstellar.jwr.one',
   integrations: [
     starlight({
       title: 'Interstellar',
