5056860cf9242d95332e057a9f85c1ccaa3eed5e
jwr1 <47087725+jwr1@users.noreply.github.com>
2025-04-17 21:45:11 -0400

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Interstellar',
+      favicon: '/favicon.png',
+      social: {
+        github: 'https://github.com/jwr1/interstellar',
+        matrix: 'https://matrix.to/#/#interstellar-space:matrix.org',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+});
